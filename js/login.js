c5764bcc477e9215f2350a3bdca3a2f389f4749e
Nermen Yehia <nermenyehia@Nermens-MacBook-Pro.local>
2023-06-02 12:09:21 +0300

    handle form validation

--- js/login.js
+++ js/login.js
@@ -1,28 +1,71 @@
-const userName = document.getElementById('userName')
+const form = document.getElementById('form');
+const username = document.getElementById('username')
 const password = document.getElementById('password')
-const loginBtn = document.getElementById('loginBtn')
+
 
  let getUser = localStorage.getItem('username')
  let getPassword = localStorage.getItem('password')
 
 
 
-loginBtn.addEventListener('click',login)
+form.addEventListener('click',login)
 
 function login(e){
     e.preventDefault()
-    checkLoginValidation()
-    checkUserData()
+    checkValidation()
+    // checkUserData()
+    if(isFormValid()){
+        checkUserData()
+        form.onsubmit();
+      }else{
+        e.preventDefault();
+      }
       
 }
 
-function checkLoginValidation(){
-    if(userName.value == "" && password.value == "") alert('Please add login data')
-
+function showError(input,message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small')
+    small.innerText = message;
 }
 
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className ='form-control success'
+}
+
+function checkLength(input,min,max){
+    if(input.value.length < min){
+      showError(input,`${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max){
+      showError(input,`password must be at less than ${max} characters`)
+    }else{
+      showSucces(input)
+    }
+  }
+  function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
+
+function checkValidation(){
+    checkLength(username,3,15)
+    checkLength(password,6,15)
+  }
+
+  function isFormValid(){
+    const inputContainers = form.querySelectorAll('.form-control');
+    let result = true;
+    inputContainers.forEach((item)=>{
+      if(item.classList.contains('error')){
+        result = false
+      }
+    })
+    return result
+  }
+
 function checkUserData(){
-    if(getUser && getUser.trim() === userName.value.trim() &&
+    if(getUser && getUser.trim() === username.value.trim() &&
     getPassword && getPassword === password.value){
     setTimeout(()=>{
         window.location = 'index.html'
--- js/register.js
+++ js/register.js
@@ -1,21 +1,60 @@
+const form = document.getElementById('form');
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const registerBtn = document.getElementById("register-btn");
-// console.log(registerBtn)
+const confirmPassword = document.getElementById("password2");
 
-registerBtn.addEventListener("click", register);
 
-function register(e) {
+form.addEventListener('submit',(e)=>{
   e.preventDefault();
-  checkValidation();
-  saveUserData();
+  checkValidation()
+  if(isFormValid()){
+    saveUserData()
+    form.onsubmit();
+  }else{
+    e.preventDefault();
+  }
+})
+
+function showError(input,message){
+  const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+function showSucces(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
-function checkValidation() {
-  if (username.value == "" && email.value == "" && password.value == "")
-    alert("Please fill the data");
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(re.test(input.value.trim())) {
+      showSucces(input)
+  }else {
+      showError(input,'Email is not invalid');
+  }
 }
+
+function checkPasswordMatch(input1, input2){
+  if(input1.value != input2.value){
+    showError(input2,'Passwords do not match')
+  }
+}
+
+function checkLength(input,min,max){
+  if(input.value.length < min){
+    showError(input,`${getFieldName(input)} must be at least ${min} characters`);
+  }else if(input.value.length > max){
+    showError(input,`password must be at less than ${max} characters`)
+  }else{
+    showSucces(input)
+  }
+}
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 
 function saveUserData() {
   localStorage.setItem("username", username.value);
@@ -24,4 +63,23 @@ function saveUserData() {
   setTimeout(() => {
     window.location = "login.html";
   }, 1500);
+ 
 }
+function checkValidation(){
+  checkLength(username,3,15)
+  checkLength(password,6,15)
+  checkEmail(email)
+  checkPasswordMatch(password,confirmPassword)
+}
+
+function isFormValid(){
+  const inputContainers = form.querySelectorAll('.form-control');
+  let result = true;
+  inputContainers.forEach((item)=>{
+    if(item.classList.contains('error')){
+      result = false
+    }
+  })
+  return result
+}
+
